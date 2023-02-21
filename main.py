5b6dc62a4ec27e0fe982556061f16ffe55dd5e62
Zavodnik10 <116554137+Zavodnik10@users.noreply.github.com>
2023-02-21 23:24:08 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+from haversine import haversine
+from geopy.geocoders import Nominatim
+import folium
+geolocator = Nominatim(user_agent="hghghg")
+def read_file(file_name):
+    
+    with open (file_name, 'r') as file:
+        s = []
+        for line in file.readlines()[13:300]:
+            s.append(line)
+
+    return s
+def dicte(year, s):
+    
+    year = str(year)
+    year = f'({year})'
+    
+    d = []
+    for i in s:
+        if year in i:
+            d.append(i)
+    for i in range (len(d)):
+        
+        d[i] = d[i].split('\t')       
+        d[i] = d[i][-1]
+        d[i] = d[i].strip('\n')
+    return d
+def vids(d, cord):
+    for i in range(len(d)):
+        d[i] = d[i].split(',')
+    a = ''
+    o = []
+    for i in d:
+        for j in i:
+            a += j
+            o.append(a)
+        a = '' 
+    
+    b = []
+    for j, i in enumerate (o):
+        try:
+            location = geolocator.geocode(i)           
+            
+            b.append((location.latitude, location.longitude))
+            
+        except :
+            continue
+    c = []
+    for j, i in enumerate(b):
+        z = haversine(i, cord)
+        c.append([i, z])
+    c = sorted(c, key = lambda x: x[1])
+    new_list = []
+
+    [new_list.append(item) for item in c if item not in new_list]
+    
+    return new_list[0:10]
+
+
+
+
+print(vids(dicte(2014, read_file('locations.list')), (7.64167, -5.52735)))
+            
